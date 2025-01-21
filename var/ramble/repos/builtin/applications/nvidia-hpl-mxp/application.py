c504b4390230860d97c9d98fa7c1bd87371bef52
Douglas Jacobsen <dwjacobsen@google.com>
2025-01-21 11:37:28 -0700

    Add nvidia-hpc-benchmarks base application and apply it
    This commit extract common logic from the NVIDIA containerized benchmark
    application definitions into a base application. This base application
    is then used to apply the same logic to each of the NVIDIA HPC Benchmark
    implementations.

--- var/ramble/repos/builtin/applications/nvidia-hpl-mxp/application.py
+++ var/ramble/repos/builtin/applications/nvidia-hpl-mxp/application.py
@@ -9,9 +9,12 @@
 from ramble.appkit import *
 
 from ramble.base_app.builtin.hpl import Hpl as HplBase
+from ramble.base_app.builtin.nvidia_hpc_benchmarks import (
+    NvidiaHpcBenchmarks as NvidiaHpcBase,
+)
 
 
-class NvidiaHplMxp(HplBase):
+class NvidiaHplMxp(HplBase, NvidiaHpcBase):
     """This application defines how to run NVIDIA's optimized version of HPL,
     which is contained in NVIDIA's HPC-Benchmarks collection.
 
@@ -48,130 +51,6 @@ class NvidiaHplMxp(HplBase):
     workload_group(
         "all_workloads",
         workloads=["standard", "calculator"],
-    )
-
-    workload_variable(
-        "internal_mpi_command",
-        default="",
-        description="MPI Command for execution using container built-in MPI",
-        workload_group="all_workloads",
-    )
-
-    workload_variable(
-        "nvshmem_disable_cuda_vmm",
-        default="1",
-        description="",
-        workload_group="all_workloads",
-    )
-    environment_variable(
-        "NVSHMEM_DISABLE_CUDA_VMM",
-        "{nvshmem_disable_cuda_vmm}",
-        description="",
-        workload_group="all_workloads",
-    )
-
-    workload_variable(
-        "hpl_fct_comm_policy",
-        default="1",
-        description="",
-        workload_group="all_workloads",
-    )
-    environment_variable(
-        "HPL_FCT_COMM_POLICY",
-        "{hpl_fct_comm_policy}",
-        description="",
-        workload_group="all_workloads",
-    )
-
-    workload_variable(
-        "hpl_use_nvshmem",
-        default="0",
-        description="Whether to use NVSHMEM or not",
-        workload_group="all_workloads",
-    )
-    environment_variable(
-        "HPL_USE_NVSHMEM",
-        "{hpl_use_nvshmem}",
-        description="Whether or not to use NVSHMEM",
-        workload_group="all_workloads",
-    )
-
-    workload_variable(
-        "hpl_p2p_as_bcast",
-        default="0",
-        description="0 = ncclBcast, 1 = ncclSend/Recv",
-        workload_group="all_workloads",
-    )
-    environment_variable(
-        "HPL_P2P_AS_BCAST",
-        "{hpl_p2p_as_bcast}",
-        description="Whether or not to use P2P for BCAST",
-        workload_group="all_workloads",
-    )
-
-    workload_variable(
-        "pmix_mca_gds",
-        default="^ds12",
-        description="",
-        workload_group="all_workloads",
-    )
-    environment_variable(
-        "PMIX_MCA_gds",
-        "{pmix_mca_gds}",
-        description="PMIX MCA gds",
-        workload_group="all_workloads",
-    )
-
-    workload_variable(
-        "ompi_mca_btl",
-        default="^vader,tcp,openib,uct",
-        description="",
-        workload_group="all_workloads",
-    )
-    environment_variable(
-        "OMPI_MCA_btl",
-        "{ompi_mca_btl}",
-        description="OpenMPI MCA btl",
-        workload_group="all_workloads",
-    )
-
-    workload_variable(
-        "ompi_mca_pml",
-        default="ucx",
-        description="",
-        workload_group="all_workloads",
-    )
-    environment_variable(
-        "OMPI_MCA_pml",
-        "{ompi_mca_pml}",
-        description="OpenMPI MCA pml",
-        workload_group="all_workloads",
-    )
-
-    workload_variable(
-        "ucx_net_devices",
-        default="enp6s0,enp12s0,enp134s0,enp140s0",
-        description="",
-        workload_group="all_workloads",
-    )
-    environment_variable(
-        "UCX_NET_DEVICES",
-        "{ucx_net_devices}",
-        description="UCX Net Devices",
-        workload_group="all_workloads",
-    )
-
-    workload_variable(
-        "ucx_max_rndv_rails",
-        default="4",
-        description="",
-        workload_group="all_workloads",
-    )
-    environment_variable(
-        "UCX_MAX_RNDV_RAILS",
-        "{ucx_max_rndv_rails}",
-        description="UCX MAximum RNDV Rails",
-        workload_group="all_workloads",
     )
 
     workload_variable(
