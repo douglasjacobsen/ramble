2542856c997af692ecd858ca56dc926e1193f158
Douglas Jacobsen <dwjacobsen@google.com>
2025-03-14 10:08:21 -0600

    Add missing join_separator to formatted executable language

--- lib/ramble/ramble/language/shared_language.py
+++ lib/ramble/ramble/language/shared_language.py
@@ -546,7 +546,9 @@ def register_template(
 
 
 @shared_directive("formatted_executables")
-def formatted_executable(name: str, prefix: str, indentation: int, commands: list):
+def formatted_executable(
+    name: str, prefix: str, indentation: int, join_separator: str, commands: list
+):
     """Define a new formatted execution for this object
 
     Args:
@@ -560,6 +562,7 @@ def formatted_executable(name: str, prefix: str, indentation: int, commands: lis
         obj.formatted_executables[name] = {
             "prefix": prefix,
             "indentation": indentation,
+            "join_separator": join_separator,
             "commands": commands.copy(),
         }
 
